0bc9e24302361c8d53ca48aff993546d5f215c62
Tangming <infly123@sina.com>
2019-02-27 08:22:23 +0800

    add project

---
+++ src/src/main/java/com/tm/hadoop/hadooplearn/HadoopLearnApplication.java
@@ -0,0 +1,13 @@
+package com.tm.hadoop.hadooplearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HadoopLearnApplication.class, args);
+    }
+
+}
